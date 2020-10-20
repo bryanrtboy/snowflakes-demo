f0dfb27c93d7c61154cede5fdfb70c6e44f4e282
Bryan <bryanrtboy@users.noreply.github.com>
2020-10-20 16:52:22 -0600

    Moved a folder

--- src/main.js
+++ src/main.js
@@ -1,72 +1,73 @@
+//Now I've made a change for sure.
 let font,
-	fontsize = 40;
+  fontsize = 40;
 
 function preload() {
-	// Ensure the .ttf or .otf font stored in the assets directory
-	// is loaded before setup() and draw() are called
-	//font = loadFont('AmaticSC-Regular.ttf');
+  // Ensure the .ttf or .otf font stored in the assets directory
+  // is loaded before setup() and draw() are called
+  //font = loadFont('AmaticSC-Regular.ttf');
 }
 
 let snowflakes = []; // array to hold snowflake objects
 
 function setup() {
-	createCanvas(400, 600);
-	fill(240);
-	noStroke();
+  createCanvas(400, 600);
+  fill(240);
+  noStroke();
 
-	// Set text characteristics
-	//textFont(font);
-	textSize(fontsize);
-	textAlign(CENTER, CENTER);
+  // Set text characteristics
+  //textFont(font);
+  textSize(fontsize);
+  textAlign(CENTER, CENTER);
 }
 
 function draw() {
-	background('brown');
-	let t = frameCount / 60; // update time
+  background("brown");
+  let t = frameCount / 60; // update time
 
-	// create a random number of snowflakes each frame
-	for (let i = 0; i < random(5); i++) {
-		snowflakes.push(new snowflake()); // append snowflake object
-	}
+  // create a random number of snowflakes each frame
+  for (let i = 0; i < random(5); i++) {
+    snowflakes.push(new snowflake()); // append snowflake object
+  }
 
-	// loop through snowflakes with a for..of loop
-	for (let flake of snowflakes) {
-		flake.update(t); // update snowflake position
-		flake.display(); // draw snowflake
-	}
+  // loop through snowflakes with a for..of loop
+  for (let flake of snowflakes) {
+    flake.update(t); // update snowflake position
+    flake.display(); // draw snowflake
+  }
 
-	updateWords();
+  updateWords();
 }
 
 // snowflake class
 function snowflake() {
-	// initialize coordinates
-	this.posX = 0;
-	this.posY = random(-50, 0);
-	this.initialangle = random(0, 2 * PI);
-	this.size = random(2, 5);
+  // initialize coordinates
+  this.posX = 0;
+  this.posY = random(-50, 0);
+  this.initialangle = random(0, 2 * PI);
+  this.size = random(2, 5);
 
-	// radius of snowflake spiral
-	// chosen so the snowflakes are uniformly spread out in area
-	this.radius = sqrt(random(pow(width / 2, 2)));
+  // radius of snowflake spiral
+  // chosen so the snowflakes are uniformly spread out in area
+  this.radius = sqrt(random(pow(width / 2, 2)));
 
-	this.update = function(time) {
-		// x position follows a circle
-		let w = 0.6; // angular speed
-		let angle = w * time + this.initialangle;
-		this.posX = width / 2 + this.radius * sin(angle);
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posX = width / 2 + this.radius * sin(angle);
 
-		// different size snowflakes fall at slightly different y speeds
-		this.posY += pow(this.size, 0.5);
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.5);
 
-		// delete snowflake if past end of screen
-		if (this.posY > height) {
-			let index = snowflakes.indexOf(this);
-			snowflakes.splice(index, 1);
-		}
-	};
+    // delete snowflake if past end of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
+  };
 
-	this.display = function() {
-		ellipse(this.posX, this.posY, this.size);
-	};
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size);
+  };
 }
